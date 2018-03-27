586e158840d0e34c521603b6d2cc7f3933a8405a
hogyun3709 <hogyu3709@gmail.com>
2018-03-27 13:30:25 +0900

    folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { hot } from 'react-hot-loader'
-import './App.css';
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "../styles/App.css";
 
 class App extends Component {
   render() {
@@ -10,12 +10,18 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          Hi Hello my name is
+          To get started, edit <code>src/App.js</code> and save to reload. Hi
+          Hello my name is hogyun
         </p>
+        <a className="button is-primary">Primary</a>
+        <a className="button is-link">Link</a>
+        <a className="button is-info">Info</a>
+        <a className="button is-success">Success</a>
+        <a className="button is-warning">Warning</a>
+        <a className="button is-danger">Danger</a>
       </div>
     );
   }
 }
 
-export default hot(module)(App) ;
+export default hot(module)(App);
